026dc9622d8935f6f66fb20afd66d19b02ada2ad
Viktor Bocz <beviktor95@gmail.com>
2020-10-14 13:30:00 +0200

    Setup of microservice architecture

--- lotr-service/src/main/java/com/codecool/lotrservice/controller/CharacterController.java
+++ lotr-service/src/main/java/com/codecool/lotrservice/controller/CharacterController.java
@@ -2,13 +2,11 @@ package com.codecool.lotrservice.controller;
 
 import com.codecool.lotrservice.component.RemoteURLReader;
 import org.codehaus.jettison.json.JSONException;
-import org.codehaus.jettison.json.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import springfox.documentation.spring.web.json.Json;
 
 import java.io.IOException;
 
@@ -23,11 +21,7 @@ public class CharacterController {
     private String urlEndPoint = "https://the-one-api.dev/v2/character";
 
     @GetMapping
-    public Json getCharacters() throws IOException, JSONException {
-        String result = remoteURLReader.readFromUrl(urlEndPoint);
-        JSONObject resultJson = new JSONObject(result);
-        resultJson.put("status", "200 OK");
-        System.out.println(resultJson);
-        return new Json(resultJson.toString());
+    public String getCharacters() throws IOException {
+        return remoteURLReader.readFromUrl(urlEndPoint);
     }
 }
